8b99534f5bf38c3fed6323df483ec26ea891a7cd
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-09-17 18:42:02 +0200

    Refactorings

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.aspectj:aspectjweaver:1.9.22.1")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
