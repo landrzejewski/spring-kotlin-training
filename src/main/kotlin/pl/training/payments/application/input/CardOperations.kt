8e556a2def4d886a2b96904e8e666a16a78b6bc2
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-09-17 10:48:22 +0200

    Refactorings

--- src/main/kotlin/pl/training/payments/application/input/CardOperations.kt
+++ src/main/kotlin/pl/training/payments/application/input/CardOperations.kt
@@ -6,6 +6,7 @@ import pl.training.payments.domain.Money
 interface CardOperations {
 
     fun inflow(cardNumber: CardNumber, amount: Money)
+
     fun payment(cardNumber: CardNumber, amount: Money)
 
 }
