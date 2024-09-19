b6d249043ffff4bf57b4fcb4d69adaed1c5f1dfe
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-09-19 11:54:38 +0200

    r

--- src/main/kotlin/pl/training/payments/Application.kt
+++ src/main/kotlin/pl/training/payments/Application.kt
@@ -1,14 +1,14 @@
 package pl.training.payments
 
-import pl.training.payments.domain.Card
-import pl.training.payments.domain.CardId
 import pl.training.payments.adapters.input.CardViewModel.Companion.CARD_NUMBER
 import pl.training.payments.adapters.input.CardViewModel.Companion.CURRENCY
+import pl.training.payments.domain.Card
+import pl.training.payments.domain.CardId
 import java.time.LocalDate
 
 fun main() {
     // Initialization
-    val card = Card(id = CardId(1), number = CARD_NUMBER, expiration = LocalDate.now().plusYears(1), currency = CURRENCY)
+    val card = Card(id = CardId(), number = CARD_NUMBER, expiration = LocalDate.now().plusYears(1), currency = CURRENCY)
     ApplicationConfiguration.cardRepository.save(card)
     val viewModel = ApplicationConfiguration.paymentsViewModel()
 
