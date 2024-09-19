4982f994d2f65beb3c0666959893e8d0d1f791d7
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-09-19 11:56:48 +0200

    r

--- src/main/kotlin/pl/training/payments/Application.kt
+++ src/main/kotlin/pl/training/payments/Application.kt
@@ -4,12 +4,12 @@ import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import pl.training.payments.application.output.CardRepository
-import pl.training.payments.domain.Card
-import pl.training.payments.domain.CardId
 import pl.training.payments.adapters.input.CardViewModel
 import pl.training.payments.adapters.input.CardViewModel.Companion.CARD_NUMBER
 import pl.training.payments.adapters.input.CardViewModel.Companion.CURRENCY
+import pl.training.payments.application.output.CardRepository
+import pl.training.payments.domain.Card
+import pl.training.payments.domain.CardId
 import java.time.LocalDate
 
 @SpringBootApplication
@@ -18,7 +18,7 @@ class Application(private val cardRepository: CardRepository, private val viewMo
 
     override fun run(args: ApplicationArguments?) {
         // Initialization
-        val card = Card(id = CardId(1), number = CARD_NUMBER, expiration = LocalDate.now().plusYears(1), currency = CURRENCY)
+        val card = Card(id = CardId(), number = CARD_NUMBER, expiration = LocalDate.now().plusYears(1), currency = CURRENCY)
         cardRepository.save(card)
 
         // Application logic
